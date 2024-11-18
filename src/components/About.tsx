5358702bc8bc3ad5e51fd10e42adb4dda9009cc0
aditya <themadtitan08@gmail.com>
2024-11-18 09:53:26 +0530

    updated About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,21 @@
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className="bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-4-xl mx-auto">
+        <h2 className="text-3xl font-extrabold text-gray-900 mb-6">
+          About Aditya
+        </h2>
+        <p className="text-lg text-gray-700">
+          Loading soon
+        </p>
+        <p className="mt-4 text-lg text-gray-700">
+          loading soon 2
+        </p>
+
+      </div>
+      
+    </section>
   )
 }
 
