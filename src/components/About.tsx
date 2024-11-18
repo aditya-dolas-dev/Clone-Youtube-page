cd6b179439c04e078e488d482947df3bc9adbc9d
aditya <themadtitan08@gmail.com>
2024-11-18 10:05:18 +0530

    slight changes updated in About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,15 +3,19 @@
 const About = () => {
   return (
     <section className="bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-4-xl mx-auto">
+      <div className="max-w-4xl mx-auto">
         <h2 className="text-3xl font-extrabold text-gray-900 mb-6">
           About Aditya
         </h2>
         <p className="text-lg text-gray-700">
-          Loading soon
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, quidem assumenda at voluptatem commodi autem eius. Obcaecati quaerat, quia, repellat dolores neque consequuntur a voluptas quidem esse debitis sit hic?
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum quae praesentium debitis consectetur non, maiores commodi esse, enim quasi ipsum, quam fugiat laboriosam labore corporis deserunt illum inventore odit aut?
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio ex aperiam facere sapiente eligendi libero dolor voluptatem ad labore incidunt iusto, repudiandae vel perspiciatis cupiditate delectus ipsam tenetur? Totam, saepe?
         </p>
         <p className="mt-4 text-lg text-gray-700">
-          loading soon 2
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, quidem assumenda at voluptatem commodi autem eius. Obcaecati quaerat, quia, repellat dolores neque consequuntur a voluptas quidem esse debitis sit hic?
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum quae praesentium debitis consectetur non, maiores commodi esse, enim quasi ipsum, quam fugiat laboriosam labore corporis deserunt illum inventore odit aut?
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio ex aperiam facere sapiente eligendi libero dolor voluptatem ad labore incidunt iusto, repudiandae vel perspiciatis cupiditate delectus ipsam tenetur? Totam, saepe?s
         </p>
 
       </div>
